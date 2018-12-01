e037e9c457b04ab82cdfa8c735ae044fc22b19c6
John P <john.pickett@gmail.com>
2018-12-01 09:57:18 -0600

    Updated mongo api and scripts

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 mongoose.promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/CRM', { useNewUrlParser: true }).then( () => {
-    console.log('Connected to MongoDB successfully');
+    if (process.env.MONGODB_URI) {
+        console.log(`Connected to MongoDB successfully at ${process.env.MONGODB_URI}`);
+    } else {
+        console.log('Connected to MongoDB at localhost');
+    }
+    
 }, (err) => {
     console.log(`Error: ${err}`);
 });
